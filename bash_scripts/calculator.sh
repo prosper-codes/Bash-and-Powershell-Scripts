fe9657b644e920297bd916e740885b9f66714f4d
prosper-codes <tpjn02@gmail.com>
2026-01-09 02:26:36 +0100

    Modify calculator to use  symbol

--- bash_scripts/calculator.sh
+++ bash_scripts/calculator.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-operation=$1
-num_1=$2
-num_2=$3
+#operation=$1
+#num_1=$2
+#num_2=$3
 
-result=$(( num_1 $operation num_2 ))
+#result=$(( num_1 $operation num_2 ))
 
-echo "you are performing $operation"
-echo "your result is $result"
-
-
+#echo "you are performing $operation"
+#echo "your result is $result"
+echo  $(( $@ ))
 
